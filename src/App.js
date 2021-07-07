30f1ce7464d05199308478abc22060e660670ba0
itzhackB <yzikbynesai@gmail.com>
2021-07-07 15:13:36 +0300

    nothing to say

--- src/App.js
+++ src/App.js
@@ -16,24 +16,14 @@ import Header from './components/Features/Header/Header';
 import Sidebar from './components/Features/Sidebar/Sidebar';
 import Footer from './components/Features/Footer/Footer';
 import Approuter from './AppRouter/Approuter';
-import Home from './components/Pages/Home'
-import Chat from './components/Pages/Chat'
-import Signup from './components/Pages/Signup'
-import Login from './components/Pages/Login'
-import {auth} from './components/services/Firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
-
-
-
 
 
 
 
 function App() {
-// const [user]=useAuthState(auth)
+
   return (
-    // <div className="App">
-    //   {user?<Chat/>:<Signup/>}
+
 
       <div id="page-content-wrapper">
         <div class="content-header">
